c7a892f1e829ced96d2f6099b11d611840d65fc4
anubhav <anubhavbisht98@gmail.com>
2020-11-02 18:28:03 +0530

    Corornavirus Prediction Project

---
+++ twiliocredential.py
@@ -0,0 +1,10 @@
+# in this file you enter your credentials which can be used for sending
+# messages
+
+# here enter your unique sid number from twilio site
+my_sid = 'xxxxx'
+# here enter your unique authorised token from twilio site
+my_auth_token = 'xxxxxx'
+# here enter the reciever mobile number which has to be verified by your twilio account beforehand
+my_cell = 'xxxx'
+my_twilio = 'xxxxx'  # here enter your unique twilio number which acts as sender
